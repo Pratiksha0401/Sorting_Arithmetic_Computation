f94d0ebb0bab51696649c34a50d6b49c6691620d
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-27 16:10:29 +0530

    Added : code of compute c+a/b

--- arithmetic_computaion_sorting.sh
+++ arithmetic_computaion_sorting.sh
@@ -4,6 +4,8 @@ read -p "Enter 1st No. a :- " a
 read -p "Enter 2nd No. b :- " b
 read -p "Enter 3rd No. c :- " c
 result1=$(($a+$b*$c))
-echo "$result1"
+echo "a+b*c= $result1"
 result2=$(($a*$b+$c))
-echo "$result2"
+echo "a*b+c= $result2"
+result3=$(($c+$a/$b))
+echo "c+a/b= $result3"
