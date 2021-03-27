22f7d540313c693ce761418f95d1416d09c09a6b
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-27 16:02:45 +0530

    Added : code of compute a+b*c

--- arithmetic_computaion_sorting.sh
+++ arithmetic_computaion_sorting.sh
@@ -3,4 +3,5 @@ echo "This problem computes different arithmetic expressions and Sorts the resul
 read -p "Enter 1st No. a :- " a
 read -p "Enter 2nd No. b :- " b
 read -p "Enter 3rd No. c :- " c
-
+result1=$(($a+$b*$c))
+echo "$result1"
