f003f3032d8583e2bb2e703a95a9c3b9b5b1d897
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-27 15:58:17 +0530

    Added: code to take three inputs

--- arithmetic_computaion_sorting.sh
+++ arithmetic_computaion_sorting.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -x
 echo "This problem computes different arithmetic expressions and Sorts the results"
+read -p "Enter 1st No. a :- " a
+read -p "Enter 2nd No. b :- " b
+read -p "Enter 3rd No. c :- " c
+
