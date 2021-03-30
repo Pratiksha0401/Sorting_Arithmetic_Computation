55a8a94f4e67bf32669a39bf27c637a213e0c38d
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-30 15:23:04 +0530

    Added: code to store results in dictionary

--- arithmetic_computaion_sorting.sh
+++ arithmetic_computaion_sorting.sh
@@ -1,16 +1,23 @@
-#!/bin/bash -x
+
+declare -A computation
 echo "This problem computes different arithmetic expressions and Sorts the results"
 read -p "Enter 1st No. a :- " a
 read -p "Enter 2nd No. b :- " b
 read -p "Enter 3rd No. c :- " c
+
 result1=$(($a+$b*$c))
-echo "a+b*c= $result1"
+computation[a+b*c]="$result1"
 
 result2=$(($a*$b+$c))
-echo "a*b+c= $result2"
+computation[a*b+c]="$result2"
 
 result3=$(($c+$a/$b))
-echo "c+a/b= $result3"
+computation[c+a/b]="$result3"
 
 result4=$(($a%$b+$c))
-echo "a%b+c=$result4"
+computation[a%b+c]="$result4"
+
+echo "(a+b*c)=${computation[a+b*c]}"
+echo "(a*b+c)=${computation[a*b+c]}"
+echo "(c+a/b)=${computation[c+a/b]}"
+echo "(a%b+c)=${computation[a%b+c]}"
