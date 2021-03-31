8d071704c7fd2c319121b726c214cb7aaabbef1b
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-03-31 14:43:21 +0530

    Added: code to get value into array

--- arithmetic_computaion_sorting.sh
+++ arithmetic_computaion_sorting.sh
@@ -1,23 +1,27 @@
-
+#!/bin/bash -x
 declare -A computation
+declare -a array1
 echo "This problem computes different arithmetic expressions and Sorts the results"
 read -p "Enter 1st No. a :- " a
 read -p "Enter 2nd No. b :- " b
 read -p "Enter 3rd No. c :- " c
 
 result1=$(($a+$b*$c))
-computation[a+b*c]="$result1"
+computation[0]="$result1"
 
 result2=$(($a*$b+$c))
-computation[a*b+c]="$result2"
+computation[1]="$result2"
 
 result3=$(($c+$a/$b))
-computation[c+a/b]="$result3"
+computation[2]="$result3"
 
 result4=$(($a%$b+$c))
-computation[a%b+c]="$result4"
+computation[3]="$result4"
 
-echo "(a+b*c)=${computation[a+b*c]}"
-echo "(a*b+c)=${computation[a*b+c]}"
-echo "(c+a/b)=${computation[c+a/b]}"
-echo "(a%b+c)=${computation[a%b+c]}"
+echo "(a+b*c)=${computation[0]}"
+echo "(a*b+c)=${computation[1]}"
+echo "(c+a/b)=${computation[2]}"
+echo "(a%b+c)=${computation[3]}"
+
+array1=( [0]=$result1 [1]=$result2 [2]=$result3 [3]=$result4 )
+echo ${array1[@]}
